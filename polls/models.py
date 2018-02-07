fd279df6bca4f72166f67b689e84cecf6cdbedd3
serhSK <sergeyskotnitskiy@gmail.com>
2018-02-07 21:34:10 +0200

    added models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Full name')
+    date_of_birth = models.DateField(verbose_name='Date of birth')
+    def __str__(self):
+        return '%s'%(self.name)
+
+
+class Group(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    group_name = models.CharField(max_length=100, verbose_name='Group name')
+    def __str__(self):
+        return '%s'%(self.group_name)
