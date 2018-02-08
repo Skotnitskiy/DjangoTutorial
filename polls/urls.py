130c50cb41189d4db1f673aae2b0bd17ad82bcf1
serhSK <sergeyskotnitskiy@gmail.com>
2018-02-08 21:08:03 +0200

    add polls namespace

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from polls import views
 
+app_name = 'polls'
 urlpatterns = [
     path('student/<int:student_id>/', views.student_detail, name='student_detail'),
     path('<str:group_name>/', views.students, name='students'),
