f279736423a03222905d8d49aceac88daefeac69
serhSK <sergeyskotnitskiy@gmail.com>
2018-02-09 04:21:54 +0200

    add new student to db

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,5 @@ app_name = 'polls'
 urlpatterns = [
     path('student/<int:student_id>/', views.student_detail, name='student_detail'),
     path('<str:group_name>/', views.students, name='students'),
+    path('students/add_student/', views.add_student, name='add_student'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 
-from polls.models import Student
+from datetime import datetime
+
+from polls.models import Student, Group
 
 
 def students(request, group_name):
     students = Student.objects.filter(group__group_name=group_name)
-    context = {'students': students}
+    groups = Group.objects.all()
+    context = {'students': students,
+               'groups': groups
+               }
     return render(request, 'polls/students.html', context)
 
 
 def student_detail(request, student_id):
-    student = Student.objects.filter(pk=student_id).first()
+    student = Student.objects.get(pk=student_id)
     context = {'student': student}
     return render(request, 'polls/student_detail.html', context)
+
+
+def add_student(request):
+    student_name = request.POST.get('name')
+    group_id = request.POST.get('group')
+    birthday = datetime.today()
+    new_sudent = Student(name=student_name, date_of_birth=birthday, group_id=group_id)
+    new_sudent.save()
+    return redirect(request.META['HTTP_REFERER'])
